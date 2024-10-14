ccc44ed880065f6c1210952f19bd964500d5d678
JosueMG15 <159586895+JosueMG15@users.noreply.github.com>
2024-10-14 07:16:50 -0600

    Agregar archivos de proyecto.

---
+++ DobbleServicio/ServicioImplementacion.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DobbleServicio
+{
+    public class ServicioImplementacion : IGestionJugador
+    {
+        public bool Registro()
+        {
+            Console.WriteLine("Hola");
+            return true;
+        }
+    }
+}
