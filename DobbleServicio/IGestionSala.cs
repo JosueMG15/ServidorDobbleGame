5579745b48af67a6800e48e85d2d7d510f278a1f
JosueMG15 <159586895+JosueMG15@users.noreply.github.com>
2024-10-15 10:23:16 -0600

    Implementación chat

---
+++ DobbleServicio/IGestionSala.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace DobbleServicio
+{
+    // NOTA: puede usar el comando "Cambiar nombre" del menú "Refactorizar" para cambiar el nombre de interfaz "IIGestionSala" en el código y en el archivo de configuración a la vez.
+    [ServiceContract(CallbackContract = typeof(ISalaCallback))]
+    public interface IGestionSala
+    {
+        [OperationContract(IsOneWay = true)]
+        void EnviarMensajeSala(String mensaje);
+    }
+
+    [ServiceContract]
+    interface ISalaCallback
+    {
+        [OperationContract(IsOneWay = true)]
+        void SalaResponse(String respuesta);
+    }
+}
--- DobbleServicio/ServicioImplementacion.cs
+++ DobbleServicio/ServicioImplementacion.cs
@@ -1,17 +1,35 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.ServiceModel;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace DobbleServicio
 {
-    public class ServicioImplementacion : IGestionJugador
+    public partial class ServicioImplementacion : IGestionJugador
     {
         public bool Registro()
         {
             Console.WriteLine("Hola");
             return true;
+        }
+    }
+
+    [ServiceBehavior(ConcurrencyMode = ConcurrencyMode.Reentrant)]
+    public partial class ServicioImplementacion : IGestionSala
+    {
+        public void EnviarMensajeSala(string mensaje)
+        {
+            try
+            {
+                OperationContext.Current.GetCallbackChannel<ISalaCallback>().SalaResponse(mensaje);
+            } 
+            catch(Exception ex) 
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+            }
+            
         }
     }
 }
