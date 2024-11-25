20ab1d0706c49d79182f22795661e404e1b1cf7e
Killerresh <erickutrera47@gmail.com>
2024-11-25 13:07:10 -0600

    .

--- DobbleServicio/IGestionAmigosCallback.cs
+++ DobbleServicio/IGestionAmigosCallback.cs
@@ -28,5 +28,8 @@ namespace DobbleServicio
 
         [OperationContract(IsOneWay = true)]
         void NotificarSalida(string nombreUsuario);
+
+        [OperationContract(IsOneWay = true)]
+        void NotificarVentanaInvitacion(string nombreUsuario);
     }
 }
